765caf92baa26fa69b934c020cfafff51d82963d
Thomas <hoernerth@gmail.com>
2018-01-25 16:26:48 +0100

    OrderedCrossover + Test

--- src/crossover/OrderedCrossover.java
+++ src/crossover/OrderedCrossover.java
@@ -1,13 +1,48 @@
 package crossover;
 
+import base.City;
 import base.Tour;
+import random.MersenneTwisterFast;
 
+import java.util.ArrayList;
+
+/**
+ * Implementation of the OrderedCrossover algorithm
+ */
 public class OrderedCrossover implements ICrossover {
-    public Tour doCrossover(Tour tour01,Tour tour02) {
-        return null;
+
+    private static MersenneTwisterFast mtF = new MersenneTwisterFast();
+
+    public Tour doCrossover(Tour tour01, Tour tour02) {
+
+        int citiesCount = tour01.getSize();
+        int startIndex = mtF.nextInt(0, citiesCount - 2);
+        int length = mtF.nextInt(1, citiesCount - startIndex - 1);
+
+        ArrayList<City> newCities = new ArrayList<>(tour02.getCities()); //workaround initial capacity
+        ArrayList<City> citiesToAdd = new ArrayList<>(tour02.getCities());
+
+        for (int i = startIndex; i < startIndex + length; i++) {
+            //copy city from tour01
+            newCities.set(i, tour01.getCity(i));
+            //check for duplicate cities in parent2
+            if (citiesToAdd.contains(newCities.get(i)))
+                citiesToAdd.remove(newCities.get(i));
+        }
+
+        for (int i = 0; i < citiesCount; i++)
+            if (i < startIndex || i > startIndex + length)
+                newCities.set(i, citiesToAdd.remove(0));
+
+        //create and return new tour
+        Tour newTour = new Tour();
+        newTour.setCities(newCities);
+        return newTour;
     }
 
+    @Override
     public String toString() {
         return getClass().getSimpleName();
     }
+
 }
--- src/test/crossover/OrderedCrossover.java
+++ src/test/crossover/OrderedCrossover.java
@@ -1,15 +1,32 @@
 package test.crossover;
 
-import base.City;
 import base.Tour;
+import crossover.ICrossover;
+import org.junit.Assert;
 import org.junit.Test;
+import test.TestTourHelper;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class OrderedCrossover {
 
+    /**
+     * Tests if the order of city-ids is the not same after the crossover has been executed
+     */
     @Test
-    public void doCrossover(Tour tour01, Tour tour02) {
+    public void doCrossover() {
+        ICrossover orderedCrossover = new crossover.OrderedCrossover();
+
+        Tour tour01 = new Tour();
+        tour01.setCities(HelperMethods.generateCities(10));
+        Tour tour02 = new Tour();
+        tour02.setCities(HelperMethods.generateCities(10));
+
+        List<Integer> idsBefore = TestTourHelper.getTourCityIds(tour01);
+        Tour newTour = orderedCrossover.doCrossover(tour01, tour02);
+        List<Integer> idsAfter = TestTourHelper.getTourCityIds(newTour);
+
+        Assert.assertNotEquals(idsAfter, idsBefore);
     }
 
 }
