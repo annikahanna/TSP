82afc7e0e3caeee181d635efb9fad4399985c672
j0-s3f <j0_s3f@web.de>
2018-01-25 15:20:44 +0100

    Finished CycleCrossover
    Unit Test failed komisch

--- src/crossover/CycleCrossover.java
+++ src/crossover/CycleCrossover.java
@@ -1,10 +1,40 @@
 package crossover;
 
+import base.City;
 import base.Tour;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CycleCrossover implements ICrossover {
-    public Tour doCrossover(Tour tour01,Tour tour02) {
-        return null;
+    public Tour doCrossover(Tour tour01,Tour tour02)
+    {
+        ArrayList<City> newTourArray = new ArrayList<>();
+        for(int i =0; i < tour01.getSize(); i++)
+        {
+            newTourArray.add(i, new City(-1,0,0));
+        }
+        int index = 0;
+        List<City> citiesLeft = (List<City>)tour02.getCities().clone();
+        int citiesLeftIndex =0;
+        while(newTourArray.get(index).getId()==-1)
+        {
+            newTourArray.set(index, tour01.getCity(index));
+            citiesLeft.remove(tour01.getCity(index));
+            City cityForNextIndex = tour02.getCities().get(index);
+            index = tour01.getCities().indexOf(cityForNextIndex);
+        }
+
+        for (City city: newTourArray) {
+           if(city.getId()==-1)
+           {
+               newTourArray.set(newTourArray.indexOf(city), citiesLeft.get(citiesLeftIndex++));
+           }
+        }
+
+        Tour newTour = new Tour();
+        newTour.setCities(newTourArray);
+        return newTour;
     }
 
     public String toString() {
--- src/test/crossover/CycleCrossover.java
+++ src/test/crossover/CycleCrossover.java
@@ -1,4 +1,66 @@
 package test.crossover;
 
+import base.City;
+import base.Tour;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class CycleCrossover {
+    @Test
+    public void testDoCrossover(){
+
+        City[] cities = new City[9];
+        for(int i=0;i<9;i++)
+        {
+            cities[i] = new City(i+1,1,1);
+        }
+
+        ArrayList<City> tourList01 = new ArrayList<>();
+        tourList01.add(cities[1-1]);
+        tourList01.add(cities[2-1]);
+        tourList01.add(cities[3-1]);
+        tourList01.add(cities[4-1]);
+        tourList01.add(cities[5-1]);
+        tourList01.add(cities[6-1]);
+        tourList01.add(cities[7-1]);
+        tourList01.add(cities[8-1]);
+        tourList01.add(cities[9-1]);
+
+        ArrayList<City> tourList02 = new ArrayList<>();
+        tourList02.add(cities[4-1]);
+        tourList02.add(cities[1-1]);
+        tourList02.add(cities[2-1]);
+        tourList02.add(cities[8-1]);
+        tourList02.add(cities[7-1]);
+        tourList02.add(cities[6-1]);
+        tourList02.add(cities[9-1]);
+        tourList02.add(cities[3-1]);
+        tourList02.add(cities[5-1]);
+
+        ArrayList<City> tourListRes = new ArrayList<>();
+        tourListRes.add(cities[1-1]);
+        tourListRes.add(cities[2-1]);
+        tourListRes.add(cities[3-1]);
+        tourListRes.add(cities[4-1]);
+        tourListRes.add(cities[7-1]);
+        tourListRes.add(cities[6-1]);
+        tourListRes.add(cities[9-1]);
+        tourListRes.add(cities[8-1]);
+        tourListRes.add(cities[5-1]);
+
+        Tour tour01 = new Tour();
+        tour01.setCities(tourList01);
+
+        Tour tour02 = new Tour();
+        tour02.setCities(tourList02);
+
+        Tour tourRes = new Tour();
+        tourRes.setCities(tourListRes);
+
+        crossover.CycleCrossover cc = new crossover.CycleCrossover();
+
+        Assert.assertEquals(tourRes, cc.doCrossover(tour01, tour02));
+    }
 }
