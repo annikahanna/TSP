e18dcb5a065215775ffac1781b00060389944434
Thomas <hoernerth@gmail.com>
2018-01-25 17:41:40 +0100

    bisle refactor :)

--- src/crossover/HeuristicCrossover.java
+++ src/crossover/HeuristicCrossover.java
@@ -8,51 +8,46 @@ import java.util.ArrayList;
 import java.util.TreeMap;
 
 public class HeuristicCrossover implements ICrossover {
-    public Tour doCrossover(Tour tour01,Tour tour02) {
-        //step 1
+
+    public Tour doCrossover(Tour tour01, Tour tour02) {
 
         City startCity = getRandomCity(tour01.getCities());
 
-        //step 2
         Tour childTour = new Tour();
         ArrayList<City> citiesLeft = new ArrayList<>(tour01.getCities());
         citiesLeft.remove(startCity);
         childTour.addCity(startCity);
 
-        while(!citiesLeft.isEmpty())
-        {
-            City momCity = childTour.getCity(childTour.getSize()-1);
+        while (!citiesLeft.isEmpty()) {
+            City momCity = childTour.getCity(childTour.getSize() - 1);
 
             int index = tour01.getCities().indexOf(momCity);
-            City leftNeighbour1 = tour01.getCity((index-1) < 0 ? tour01.getSize()-1 : index-1);
-            City rightNeighbour1 = tour01.getCity((index+1)> tour01.getSize()-1?0:index+1);
+            City leftNeighbour1 = tour01.getCity((index - 1) < 0 ? tour01.getSize() - 1 : index - 1);
+            City rightNeighbour1 = tour01.getCity((index + 1) > tour01.getSize() - 1 ? 0 : index + 1);
 
             index = tour02.getCities().indexOf(momCity);
-            City leftNeighbour2 = tour01.getCity((index-1) < 0 ? tour01.getSize()-1 : index-1);
-            City rightNeighbour2 = tour01.getCity((index+1)> tour01.getSize()-1?0:index+1);
+            City leftNeighbour2 = tour01.getCity((index - 1) < 0 ? tour01.getSize() - 1 : index - 1);
+            City rightNeighbour2 = tour01.getCity((index + 1) > tour01.getSize() - 1 ? 0 : index + 1);
 
             TreeMap<Double, City> map = new TreeMap<>();
-            map.put(getCityDistance(momCity, leftNeighbour1),leftNeighbour1);
-            map.put(getCityDistance(momCity, rightNeighbour1),rightNeighbour1);
-            map.put(getCityDistance(momCity, leftNeighbour2),leftNeighbour2);
-            map.put(getCityDistance(momCity, rightNeighbour2),rightNeighbour2);
+            map.put(getCityDistance(momCity, leftNeighbour1), leftNeighbour1);
+            map.put(getCityDistance(momCity, rightNeighbour1), rightNeighbour1);
+            map.put(getCityDistance(momCity, leftNeighbour2), leftNeighbour2);
+            map.put(getCityDistance(momCity, rightNeighbour2), rightNeighbour2);
 
-            City nearestNeighbour = map.pollFirstEntry().getValue();
+            //pull 1st Entry
+            City neighbour = map.pollFirstEntry().getValue();
 
-            if(!childTour.containsCity(nearestNeighbour))
-            {
-                childTour.addCity(nearestNeighbour);
-                citiesLeft.remove(nearestNeighbour);
-            }
-            else
-            {
-                nearestNeighbour = map.pollFirstEntry().getValue();
-                if(!childTour.containsCity(nearestNeighbour)) {
-                    childTour.addCity(nearestNeighbour);
-                    citiesLeft.remove(nearestNeighbour);
-                }
-                else
-                {
+            if (!childTour.containsCity(neighbour)) {
+                childTour.addCity(neighbour);
+                citiesLeft.remove(neighbour);
+            } else {
+                //poll 2nd Entry
+                neighbour = map.pollFirstEntry().getValue();
+                if (!childTour.containsCity(neighbour)) {
+                    childTour.addCity(neighbour);
+                    citiesLeft.remove(neighbour);
+                } else {
                     City randomCity = getRandomCity(citiesLeft);
                     childTour.addCity(randomCity);
                     citiesLeft.remove(randomCity);
@@ -62,20 +57,19 @@ public class HeuristicCrossover implements ICrossover {
         return childTour;
     }
 
-    private City getRandomCity(ArrayList<City> cities)
-    {
-        //TODO Singelton random
+    private static City getRandomCity(ArrayList<City> cities) {
         MersenneTwisterFast mtf = new MersenneTwisterFast();
-        int randomIndex = mtf.nextInt(0, cities.size()-1);
+        int randomIndex = mtf.nextInt(0, cities.size() - 1);
         return cities.get(randomIndex);
     }
 
-    private static double getCityDistance(City c1, City c2)
-    {
-        return Tour.euclideanDistance(c1.getX(),c1.getY(),c2.getX(),c2.getY());
+    private static double getCityDistance(City c1, City c2) {
+        return Tour.euclideanDistance(c1.getX(), c1.getY(), c2.getX(), c2.getY());
     }
 
+    @Override
     public String toString() {
         return getClass().getSimpleName();
     }
+
 }
