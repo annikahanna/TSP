abc91b025ce0bb2c37a590beae464a898a258782
MakroCow <yoco.lie@gmail.com>
2018-01-26 12:54:41 +0100

    Added tests and fixed minor issues

--- src/crossover/PartiallyMatchedCrossover.java
+++ src/crossover/PartiallyMatchedCrossover.java
@@ -3,7 +3,6 @@ package crossover;
 import base.City;
 import base.Pair;
 import base.Tour;
-import com.sun.org.apache.xpath.internal.SourceTree;
 import random.MersenneTwisterFast;
 
 import java.util.ArrayList;
@@ -11,17 +10,17 @@ import java.util.Arrays;
 
 public class PartiallyMatchedCrossover implements ICrossover {
 
-    private ArrayList<Pair> mapping = new ArrayList<Pair>();
+    private ArrayList<Pair> mapping = new ArrayList<>();
 
 
     public Tour doCrossover(Tour tour01, Tour tour02) {
         int crossoverpoint1, crossoverpoint2;
 
-        /* Zufällig 2 Crosoverpunkte wählen */
+        /* Zufällig 2 Crossoverpunkte wählen */
         MersenneTwisterFast rand = new MersenneTwisterFast();
-        crossoverpoint1 = rand.nextInt(0, tour01.getCities().size());
-        crossoverpoint2 = rand.nextInt(crossoverpoint1, tour01.getCities().size());
-        System.out.println("crossoverpoint1:" + crossoverpoint1 + " crossoverpoint2: "+ crossoverpoint2);
+        crossoverpoint1 = rand.nextInt(0, tour01.getCities().size()-1);
+        crossoverpoint2 = rand.nextInt(crossoverpoint1+1, tour01.getCities().size());
+        //System.out.println("crossoverpoint1:" + crossoverpoint1 + " crossoverpoint2: "+ crossoverpoint2);
 
         /* Die Eltern direkt in die Kinder speichern */
         ArrayList<City> tour1Cities = tour01.getCities();
@@ -36,19 +35,18 @@ public class PartiallyMatchedCrossover implements ICrossover {
             tour1Cities.set(i, temp);
         }
 
-        for (int i= 0; i<mapping.size(); i++){
+/*      for (int i= 0; i<mapping.size(); i++){
             System.out.println("Mapping: " + mapping.get(i).getFirst().toString() + mapping.get(i).getSecond().toString());
-        }
-
+        }*/
 
 
         /* Zyklen im Mapping erkennen und entfernen */
+        int value = 1;
         for (int i = 0; i < mapping.size(); i++) {
-            int value = 1;
             for (int j = value; j < mapping.size(); j++) {
                 if (mapping.get(i).getSecond() == mapping.get(j).getFirst()) {
                     mapping.get(i).setSecond(mapping.get(j).getSecond());
-                    System.out.println("mapping wird geändert: erste regel" + mapping.get(i).getSecond().toString() + " zweite regel:" + mapping.get(j).getFirst());
+                    //System.out.println("mapping wird geändert: erste regel" + mapping.get(i).getSecond().toString() + " zweite regel:" + mapping.get(j).getFirst());
                 }
             }
             value++;
@@ -56,14 +54,14 @@ public class PartiallyMatchedCrossover implements ICrossover {
 
         /* Werte außerhalb des Crossovers entsprechend ändern - Child 1*/
         for (int i = 0; i < crossoverpoint1; i++) {
-            for (int j = 1; j < mapping.size(); j++) {
-                System.out.println("j: " + j);
+            for (int j = 0; j < mapping.size(); j++) {
+                //System.out.println("child1 j: " + j);
                 if (tour1Cities.get(i) == mapping.get(j).getFirst()) {
                     tour1Cities.set(i, mapping.get(j).getSecond());
                 }
             }
         }
-        for (int i = 0; i < crossoverpoint2; i++) {
+        for (int i = crossoverpoint2; i < tour1Cities.size(); i++) {
             for (int j = 0; j < mapping.size(); j++) {
                 if (tour1Cities.get(i) == mapping.get(j).getFirst()) {
                     tour1Cities.set(i, mapping.get(j).getSecond());
@@ -73,13 +71,13 @@ public class PartiallyMatchedCrossover implements ICrossover {
         /* Werte außerhalb des Crossovers entsprechend ändern - Child 2*/
         /* Hier müssen die Paare aus dem Mapping natürlich andersherum interpretiert werden*/
         for (int i = 0; i < crossoverpoint1; i++) {
-            for (int j = 1; j < mapping.size(); j++) {
+            for (int j = 0; j < mapping.size(); j++) {
                 if (tour2Cities.get(i) == mapping.get(j).getSecond()) {
                     tour2Cities.set(i, mapping.get(j).getFirst());
                 }
             }
         }
-        for (int i = 0; i < crossoverpoint2; i++) {
+        for (int i = crossoverpoint2; i < tour2Cities.size(); i++) {
             for (int j = 0; j < mapping.size(); j++) {
                 if (tour2Cities.get(i) == mapping.get(j).getSecond()) {
                     tour2Cities.set(i, mapping.get(j).getFirst());
@@ -93,7 +91,7 @@ public class PartiallyMatchedCrossover implements ICrossover {
         Tour child2 = new Tour();
         child2.setCities(tour2Cities);
 
-        System.out.println(Arrays.toString(child1.getCities().toArray()) + Arrays.toString(child2.getCities().toArray()));
+/*        System.out.println(Arrays.toString(child1.getCities().toArray()) + "\n" + Arrays.toString(child2.getCities().toArray()));*/
 
         /* Das fittere Kind auswählen und zurückgeben*/
         if (child1.compareTo(child2) != 1) {
